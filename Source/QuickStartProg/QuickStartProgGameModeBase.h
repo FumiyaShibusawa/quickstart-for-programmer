0f10dcacd9cfd8876243225623fd5c53c101f1b9
FumiyaShibusawa <shizai.asdfghjkl@gmail.com>
2018-08-13 22:50:25 +0900

    Initial commit

---
+++ Source/QuickStartProg/QuickStartProgGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "QuickStartProgGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUICKSTARTPROG_API AQuickStartProgGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
